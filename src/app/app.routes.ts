6751fa78aca7f1f7af1aa0c2f32fd769a3c35502
Dario Schaffner <darioschaffner7@gmail.com>
2024-10-04 15:18:28 +0200

    ✨ add font and color ideas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ColorPaletteComponent } from './color-pallete/color-palette.component';
+import { FontsComponent } from './fonts/fonts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    redirectTo: 'app',
+    pathMatch: 'full',
+  },
+  {
+    path: 'colors',
+    component: ColorPaletteComponent,
+  },
+  {
+    path: 'fonts',
+    component: FontsComponent,
+  },
+];
