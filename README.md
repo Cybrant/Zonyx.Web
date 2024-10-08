87995a67cf86a4e12f189bc5899464be303af36e
Dario <63245314+dar1ooo@users.noreply.github.com>
2024-10-08 13:39:18 +0200

    ✨ base setup (#3)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Zonyx.Web
+# ZonyxWeb
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.3.
---
+++ src/app/color-pallete/color-palette.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-pallete',
+  standalone: true,
+  imports: [],
+  templateUrl: './color-palette.component.html',
+  styleUrl: './color-palette.component.scss',
+})
+export class ColorPaletteComponent {}
